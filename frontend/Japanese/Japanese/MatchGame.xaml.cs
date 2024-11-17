0bb0b19d89596842af8153c706211c37669851f0
Dmytro Piskun <dmytro.piskun@swapp.ai>
2024-11-17 19:05:53 +0200

    DPLM-6: Clean up

--- frontend/Japanese/Japanese/MatchGame.xaml.cs
+++ frontend/Japanese/Japanese/MatchGame.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Japanese
 {
@@ -17,12 +8,14 @@ namespace Japanese
     /// </summary>
     public partial class MatchGame : Window
     {
-        private Button selectedJapaneseButton = null;
-        private Button selectedTranslationButton = null;
+        private Button? selectedJapaneseButton;
+        private Button? selectedTranslationButton;
+
         public MatchGame()
         {
             InitializeComponent();
         }
+
         private void JapaneseWordButton_Click(object sender, RoutedEventArgs e)
         {
             selectedJapaneseButton = sender as Button;
@@ -39,7 +32,10 @@ namespace Japanese
         {
             if (selectedJapaneseButton != null && selectedTranslationButton != null)
             {
-                if (selectedJapaneseButton.Tag.ToString() == selectedTranslationButton.Tag.ToString())
+                if (
+                    selectedJapaneseButton.Tag.ToString()
+                    == selectedTranslationButton.Tag.ToString()
+                )
                 {
                     MessageBox.Show("Правильно!");
                     selectedJapaneseButton.IsEnabled = false;
